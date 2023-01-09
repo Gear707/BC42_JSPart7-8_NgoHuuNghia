c53526c08cfc98d7668bb0919a69d135863247ae
Nghia <nghia3977@gmail.com>
2023-01-09 17:32:09 +0700

    JS Part 7-8

--- js/main.js
+++ js/main.js
@@ -1,82 +1,75 @@
 // Tạo hàm DOM qua selector
-function queSel(selector) {
-    return document.querySelector(selector);
-}
+queSel = (selector) => document.querySelector(selector);
 
-// Khởi tạo 1 mảng rỗng
+// Khởi tạo mảng ban đầu rỗng
 const myArray = new Array();
 
 // Tạo hàm có chức năng là khi click button "Thêm số" sẽ thêm phần tử mới vào mảng
-queSel('#btnAdd').onclick = function () {
+queSel('#btnAdd').onclick = () => {
     // input
     let numN = +queSel('#numN').value;
-    myArray.push(numN); // thêm phần tử mới vào cuối mảng dựa vào giá trị user nhập vào
 
     // progress
-    queSel('#numN').value = ''; // xóa giá trị mà user đã thêm vào để tiếp tục nhập một giá trị khác
-    let result = ''; // tạo biến xuất ra các phần tử trong mảng
-    for (index = 0; index < myArray.length; index++) {
-        result += myArray[index] + ' ';  // thêm từng phần tử vào mảng dựa vào biến bước nhảy i
-    }
+    // thêm phần tử mới vào cuối mảng dựa vào giá trị user nhập vào
+    myArray.push(numN);
+    // ta có thể xóa giá trị mà user đã thêm vào để tiếp tục nhập một giá trị khác
+    queSel('#numN').value = '';
 
     // output
-    queSel('#output0').innerHTML = result;
-}
+    queSel('#output0').innerHTML = myArray;
+};
 
 
 /* 1. Tổng số dương */
-queSel('#btnSum').onclick = function () {
+queSel('#btnSum').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
     // progress
-    let sum = 0;
-    for (let index = 0; index < myArray.length; index++) {
-        if (myArray[index] > 0) {
-            sum += myArray[index];
-        }
-    }
+    // lọc ra 1 mảng mới chỉ chứa các số dương lấy từ mảng ban đầu
+    let posArray = myArray.filter(value => value > 0);
+    // thực hiện tính tổng các phần tử trong mảng mới 
+    let posSum = posArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
     // output
-    queSel('#output1').innerHTML = 'Tổng các số dương: ' + sum;
-}
+    queSel('#output1').innerHTML = 'Tổng các số dương: ' + posSum;
+};
 
 
 /* 2. Đếm số dương */
-queSel('#btnPosNum').onclick = function () {
+queSel('#btnPosNum').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
-    // progress
-    let count = 0;
-    for (let index = 0; index < myArray.length; index++) {
-        if (myArray[index] > 0) {
-            count++;
-        }
-    }
+    // progress: lọc ra 1 mảng mới chỉ chứa các số dương lấy từ mảng ban đầu
+    let posArray = myArray.filter(value => value > 0);
 
-    // output
-    queSel('#output2').innerHTML = 'Có ' + count + ' số dương';
-}
+    // output: độ dài của mảng mới phản ánh đúng số lượng số dương đang có trong mảng gốc
+    queSel('#output2').innerHTML = 'Có ' + posArray.length + ' số dương';
+};
 
 
 /* 3. Tìm số nhỏ nhất */
-queSel('#btnMin').onclick = function () {
-    // input: mảng myArray chứa các phần tử user đã nhập
-
-    // progress: dùng phương thức Math.min.apply để tìm số nhỏ nhất trong mảng
-    let minValue = Math.min.apply(null, myArray);
-
-    // output
-    queSel('#output3').innerHTML = 'Số nhỏ nhất: ' + minValue;
-}
-
-
-/* 4. Tìm số dương nhỏ nhất */
-queSel('#btnMinPos').onclick = function () {
+queSel('#btnMin').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
     // progress
-    // khởi tạo mảng mới chứa các số dương được lấy từ mảng ban đầu
-    let posArray = myArray.filter((currentValue) => currentValue > 0);
+    // C1: dùng phương thức Math.min.apply để tìm số nhỏ nhất trong mảng
+    // let minValue = Math.min.apply(null, myArray);
+
+    // C2: dùng reduce để duyệt & so sánh từng phần tử trong mảng
+    let minValue = myArray.reduce((previousValue, currentValue) => (previousValue < currentValue) ? previousValue : currentValue);
+
+    // output
+    queSel('#output3').innerHTML = 'Số nhỏ nhất: ' + minValue;
+};
+
+
+/* 4. Tìm số dương nhỏ nhất */
+queSel('#btnMinPos').onclick = () => {
+    // input: mảng myArray chứa các phần tử user đã nhập
+
+    // progress
+    // tạo mảng mới chứa các số dương được lấy từ mảng ban đầu
+    let posArray = myArray.filter(value => value > 0);
     let result4 = '';
     // kiểm tra mảng mới có phần tử hay không
     if (!posArray.length) {
@@ -91,18 +84,16 @@ queSel('#btnMinPos').onclick = function () {
 
     // output
     queSel('#output4').innerHTML = result4;
-}
+};
 
 
 /* 5. Tìm số chẵn cuối cùng */
-queSel('#btnLastEven').onclick = function () {
+queSel('#btnLastEven').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
     // progress
     // khởi tạo mảng mới chứa các số chẵn được lấy từ mảng ban đầu
-    let evenArray = myArray.filter(function (currentValue) {
-        return (currentValue >= 0 && currentValue % 2 === 0);
-    })
+    let evenArray = myArray.filter(value => value >= 0 && value % 2 === 0)
     let result5 = '';
     // kiểm tra mảng mới có phần tử hay không
     if (!evenArray.length) {
@@ -116,16 +107,16 @@ queSel('#btnLastEven').onclick = function () {
 
     // output
     queSel('#output5').innerHTML = result5;
-}
+};
 
 
 /* 6. Đổi vị trí hai phần tử bất kì trong mảng */
-// Khởi tạo hàm hoán đổi vị trí của 2 phần tử bất kì trong mảng
-function swapElements(array, index1, index2) {
+// Tạo hàm hoán đổi vị trí của 2 phần tử bất kì trong mảng
+const swapElements = (array, index1, index2) => {
     [array[index1], array[index2]] = [array[index2], array[index1]];
-}
+};
 
-queSel('#btnSwap').onclick = function () {
+queSel('#btnSwap').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
     // progress
@@ -133,32 +124,27 @@ queSel('#btnSwap').onclick = function () {
     let secondIndex = +queSel('#secondIndex').value;
     // gọi hàm hoán đổi vị trí
     swapElements(myArray, firstIndex, secondIndex);
-    let result6 = '';
-    // duyệt & xuất ra các phần tử của mảng sau khi hoán đổi
-    for (let index = 0; index < myArray.length; index++) {
-        result6 += myArray[index] + ' ';
-    }
 
     // output
-    queSel('#output6').innerHTML = 'Kết quả sau khi hoán đổi là: ' + result6;
-}
+    queSel('#output6').innerHTML = 'Kết quả sau khi hoán đổi là: ' + myArray;
+};
 
 
 /* 7. Sắp xếp tăng dần */
-queSel('#btnSort').onclick = function () {
+queSel('#btnSort').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
-    // progress: dùng phương thức sort & tạo hàm so sánh phần tử để sắp xếp tăng dần
+    // progress: dùng phương thức sort & tạo hàm callback để sắp xếp tăng dần
     let sortArr = myArray.sort((a, b) => a - b);
 
     // output
     queSel('#output7').innerHTML = 'Mảng sắp xếp tăng dần: ' + sortArr;
-}
+};
 
 
 /* 8. Tìm số nguyên tố đầu tiên trong mảng */
 // Tạo hàm kiểm tra số nguyên tố
-function checkPrimeNum(n) {
+checkPrimeNum = (n) => {
     if (n < 2) {
         return false;
     }
@@ -174,8 +160,9 @@ function checkPrimeNum(n) {
         }
     }
     return true;
-}
-queSel('#btnFirstPrimeNum').onclick = function () {
+};
+
+queSel('#btnFirstPrimeNum').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
     // progress
@@ -192,48 +179,37 @@ queSel('#btnFirstPrimeNum').onclick = function () {
 
     // output
     queSel('#output8').innerHTML = result8;
-}
+};
 
 
 /* 9. Đếm số nguyên trong mảng */
-queSel('#btnInteger').onclick = function () {
+queSel('#btnInteger').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
-    // progress
-    let countInt = 0; // khai báo biến đếm số nguyên
-    for (let index = 0; index < myArray.length; index++) {
-        if (Number.isInteger(myArray[index])) {
-            countInt++;
-        }
-    }
+    // progress: tạo mảng mới chỉ chứa các số nguyên được lấy từ mảng ban đầu
+    let intArray = myArray.filter(value => Number.isInteger(value))
 
-    // output
-    queSel('#output9').innerHTML = 'Số lượng các số nguyên: ' + countInt;
-}
+    // output: độ dài của mảng mới phản ánh đúng số lượng số nguyên đang có trong mảng gốc
+    queSel('#output9').innerHTML = 'Số lượng các số nguyên: ' + intArray.length;
+};
 
 
 /* 10. So sánh số lượng số âm & số dương */
-queSel('#btnCompare').onclick = function(){
+queSel('#btnCompare').onclick = () => {
     // input: mảng myArray chứa các phần tử user đã nhập
 
     // progress
-    let countPos = 0; // khai báo biến đếm số dương
-    let countNeg = 0; // khai báo biến đếm số âm
     let result10 = '';
-    // duyệt mảng để đếm số dương & số âm
-    for (let index = 0; index < myArray.length; index++) {
-        if (myArray[index] > 0) {
-            countPos++;
-        } 
-        else if (myArray[index] < 0) {
-            countNeg++;
-        }
-    }
-    // so sánh số lượng số dương & số âm
-    if (countPos > countNeg) {
+    
+    // lọc ra 1 mảng mới chỉ chứa các số dương lấy từ mảng ban đầu
+    let posArray = myArray.filter(value => value > 0);
+    // lọc ra 1 mảng mới chỉ chứa các số âm lấy từ mảng ban đầu
+    let negArray = myArray.filter(value => value < 0);
+
+    if (posArray.length > negArray.length) {
         result10 = 'Số dương > Số âm';
-    } 
-    else if (countPos < countNeg) {
+    }
+    else if (posArray.length < negArray.length) {
         result10 = 'Số dương < Số âm';
     }
     else {
@@ -242,4 +218,4 @@ queSel('#btnCompare').onclick = function(){
 
     // output
     queSel('#output10').innerHTML = result10;
-}
+};
